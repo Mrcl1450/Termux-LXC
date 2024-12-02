a573cef024e382ca1fe9057c76eb0ce86bab3ddd
Marcelo Reis <Mrcl1450@users.noreply.github.com>
2024-12-02 15:32:36 +0000

    Update pre-start.sh

--- pre-start.sh
+++ pre-start.sh
@@ -4,6 +4,8 @@ export XDG_RUNTIME_DIR=${TMPDIR}
 
 kill -9 $(pgrep -f "termux.x11") 2>/dev/null
 
+sudo chown $USER:$USER /data/data/com.termux/files/usr/tmp
+
 pulseaudio --start \
     --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" \
     --exit-idle-time=-1
