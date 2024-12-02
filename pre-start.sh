b04447a96f13f929d8bf08aa862180c9cd17a1e3
Marcelo Reis <Mrcl1450@users.noreply.github.com>
2024-12-02 12:55:23 +0000

    Update pre-start.sh

--- pre-start.sh
+++ pre-start.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
+export XDG_RUNTIME_DIR=${TMPDIR}
+
+kill -9 $(pgrep -f "termux.x11") 2>/dev/null
+
 pulseaudio --start \
     --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" \
     --exit-idle-time=-1
 
-kill -9 $(pgrep -f "termux.x11") 2>/dev/null
-
-#export XDG_RUNTIME_DIR=${TMPDIR}
-termux-x11 :0 -&
+termux-x11 :0 -ac -extension MIT-SHM &
 
 sudo mount -B "/data/data/com.termux/files/usr/var/lib/lxc/ubuntu/rootfs" "/data/data/com.termux/files/usr/var/lib/lxc/ubuntu/rootfs"
 sudo mount -i -o remount,suid "/data/data/com.termux/files/usr/var/lib/lxc/ubuntu/rootfs"
